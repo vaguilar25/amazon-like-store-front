1515086cb63cb91e1000c22d8d65829639b768af
vaguilar25 <vivian.aguilar@gmail.com>
2018-12-16 10:35:20 -0600

    add total purchase

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql");
 var Table = require('cli-table');
-var inquire = require ('inquirer');
+var inquirer = require('inquirer');
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -17,13 +17,28 @@ var connection = mysql.createConnection({
 });
 console.log("test");
 
+var purchase = function (id, price,quantity) {
+    this.id = id,
+        this.quantity,
+        this.price = price,
+        this.total = function() {
+           var total=this.price * this.quantity; 
+          console.log("The total cost of your purchase is: " + parseFloat(total).toFixed(2));
+          // return
+        }
+        
+
+}
+
+var recentOrder;
+
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     readProducts();
 });
 
-function readProducts() {
+function readProducts(quantitySelect) {
     console.log("Selecting all products...\n");
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
@@ -31,17 +46,116 @@ function readProducts() {
         //console.log(res);
 
         var table = new Table({
-            head: ['ID', 'PRODUCT', 'PRICE']
-            , colWidths: [4, 50, 10]
+            head: ['ID', 'DEPARTMENT', 'PRODUCT', 'PRICE', 'STOCK']
+            , colWidths: [4, 20, 50, 10, 10]
         });
 
         for (var i = 0; i < res.length; i++) {
 
-            table.push([res[i].item_id, res[i].product_name, parseFloat(res[i].price).toFixed(2)]);
+            table.push([res[i].item_id, res[i].department_name, res[i].product_name, parseFloat(res[i].price).toFixed(2), res[i].stock_quantity]);
 
         }
         console.log(table.toString());
 
-        connection.end();
+     // recentOrder.total();
+
+
+    });
+    
+    //if (!isNaN(recentOrder)) {
+      // recentOrder.total();
+   // }
+   // return
+}
+inquireIdPurchase();
+//recentOrder.total();
+
+
+function inquireIdPurchase() {
+    inquirer.prompt([
+        {
+            type: "Input",
+            name: "id",
+            message: "What is the id of the item you would like to purchase?"
+        }
+    ]).then(function (idSelect) {
+
+        inquireQuantityPurchase(idSelect.id);
+
+    });
+
+}
+
+function inquireQuantityPurchase(idSelect) {
+    inquirer.prompt([
+        {
+            type: "Input",
+            name: "quantity",
+            message: "How many would you like?"
+        }
+    ]).then(function (quantitySelect) {
+        checkInventory(idSelect, quantitySelect.quantity)
     });
 }
+
+function checkInventory(idSelect, quantitySelect) {
+    connection.query("SELECT product_name,stock_quantity,price FROM products where item_id = ? ",
+        [
+            idSelect
+        ]
+
+        , function (err, res, fields) {
+            if (err) throw err;
+            // Log all results of the SELECT statement
+            //console.log(res);
+            var results = res[0];
+
+            if (quantitySelect <= results.stock_quantity) {
+                var new_stock = results.stock_quantity - quantitySelect;
+                calculatePurchase(idSelect, quantitySelect,results.price);
+                updateStock(idSelect, new_stock);
+
+                readProducts();
+                
+                // if (!isNaN(recentOrder) ) {
+                //console.log("The total cost of your purchase is:" + recentOrder.total);
+                //}
+
+
+                //console.log("Your TOTAL purchase is: $" +  parseFloat(totalPurchase).toFixed(2));
+            } else {
+                console.log("Insufficient quantity!");
+            }
+
+        });
+
+}
+
+function updateStock(idSelect, new_stock) {
+    connection.query("UPDATE products set stock_quantity = ? WHERE item_id = ?",
+        [
+            new_stock,
+            idSelect
+        ],
+        function (err, res, fields) {
+            if (err) throw err;
+            // Log all results of the SELECT statement
+            //console.log(res);
+
+
+            // connection.end();
+        });
+}
+
+function calculatePurchase(id, quantitySelect, price,) {
+    //  recentOrder = new purchase(idSelect,results.price,parseFloat(totalPurchase).toFixed(2));
+   // var totalPurchase = quantitySelect * price;
+   console.log(id);
+   console.log(price);
+   console.log(quantitySelect);
+   
+    recentOrder = new purchase(id, price,quantitySelect);
+    //return recentOrder;
+
+}
+
