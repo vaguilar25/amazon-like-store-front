6f4335ecc764f8cba6da4f86ff5cbb6b33614898
vaguilar25 <vivian.aguilar@gmail.com>
2018-12-16 00:36:33 -0600

    Add display table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,39 +1,46 @@
 var mysql = require("mysql");
+var Table = require('cli-table');
 
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 8080,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "password",
-  database: "bamazon"
+    // Your password
+    password: "password",
+    database: "bamazon"
 });
 console.log("test");
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     readProducts();
-  });
-  
-  function readProducts() {
-      console.log("Selecting all products...\n");
-      connection.query("SELECT * FROM products", function(err, res) {
+});
+
+function readProducts() {
+    console.log("Selecting all products...\n");
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         // Log all results of the SELECT statement
         //console.log(res);
-  
-      for (var i=0; i< res.length;i++) {
-          console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price + " | " )
-      }
-  
-  
+
+        var table = new Table({
+            head: ['ID', 'PRODUCT', 'PRICE']
+            , colWidths: [4, 50, 10]
+        });
+
+        for (var i = 0; i < res.length; i++) {
+
+            table.push([res[i].item_id, res[i].product_name, parseFloat(res[i].price).toFixed(2)]);
+           
+        }
+        console.log(table.toString());
+
         connection.end();
-      });
-    }
-    
+    });
+}
